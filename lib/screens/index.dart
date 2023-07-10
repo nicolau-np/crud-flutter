9b5fa1b54ed339456908821f04b361e6da8316e2
nicolau-np <mr1Normaliii@gmail.com>
2023-07-10 22:17:31 +0100

    update

--- lib/screens/index.dart
+++ lib/screens/index.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:ecomap/providers/ponto_provider.dart';
+import 'package:provider/provider.dart';
 
 class IndexPage extends StatefulWidget {
   const IndexPage({super.key});
@@ -12,13 +14,38 @@ class IndexPage extends StatefulWidget {
 class _IndexPageState extends State<IndexPage> {
   @override
   Widget build(BuildContext context) {
+    final pontoProvider = Provider.of<PontoProvider>(context, listen: false);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Listar'),
       ),
       body: Column(
-        children: const [
-          Text('hello'),
+        children: [
+          const Text('hello'),
+          Consumer<PontoProvider>(
+            builder: (context, pontoProvider, _) {
+              final pontos = pontoProvider.pontos;
+
+              if (pontos.isEmpty) {
+                return const Center(
+                  child: Text('Nenhum ponto encontrado'),
+                );
+              }
+
+              return ListView.builder(
+                itemCount: pontos.length,
+                itemBuilder: (context, index) {
+                  final ponto = pontos[index];
+
+                  return ListTile(
+                    title: Text(ponto.nome),
+                    subtitle: Text(ponto.descricao),
+                  );
+                },
+              );
+            },
+          ),
         ],
       ),
     );
